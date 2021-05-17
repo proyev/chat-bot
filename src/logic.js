42362546f17733f14e34f03d2959fd73b2ec1237
proyev <prots.yevhen@gmail.com>
2021-05-18 01:17:05 +0200

    Test function and working communication

--- src/logic.js
+++ src/logic.js
@@ -1,1 +1,9 @@
 let testFunc = () => console.log('Logic is working');
+
+const testComFun = msg => {
+	let retVal;
+	console.log(msg);
+	msg.includes('hello') ? retVal = 'oh, hi' : retVal = msg;
+	console.log(retVal);
+	return retVal;
+}
