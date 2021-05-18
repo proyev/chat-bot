9f6835f80f187fca77ba8d2ea2d231cc9cc72e8a
proyev <prots.yevhen@gmail.com>
2021-05-18 21:33:32 +0200

    First logic

--- src/logic.js
+++ src/logic.js
@@ -7,3 +7,91 @@ const testComFun = msg => {
 	console.log(retVal);
 	return retVal;
 }
+
+const showID = spyStatus => {
+	if (spyStatus){
+		return personalData = {
+			identityName: 'Sam Sepiol',
+			trueName: '',
+			identityAge: 28,
+			identityBirthday: '20.09.1952',
+			contactPlaces: ['library', 'pub'],
+			age: 31,
+
+		}
+	}
+	// return
+}
+
+function checkAskedQuestions(askedQuestions, lastQuestion) {
+	askedQuestions.push(lastQuestion);
+	if (askedQuestions.length > 2){
+		gameOver('train');
+	} else if (askedQuestions[askedQuestions.length - 2] === lastQuestion){
+		gameOver('train');
+	}
+	return askedQuestions;
+}
+
+const startGame = () => {
+	//those counters will make a decision about the final result,
+	// if the player goes that far of course
+	let dizzyCounter = 0, aggressiveCounter = 0,
+		questionCounter = 0, releaseCounter = 0;
+
+	spyStatus = Math.floor(Math.random() * 2);
+	showID(spyStatus);
+
+	suspectData = {
+		name: 'Sam Sepiol',
+		nick: 'Sal',
+		birth: '20.09.1952',
+		age: 28,
+		prof: '',
+		friends: '',
+		hobbys: ['painting', 'reading', ''],
+
+	}
+
+	const askedQuestions = [];
+
+	interrogatorQuestionnaire = {
+		'State your name': {
+			answer: 'Sam Sepiol',
+
+		}
+		// 'State your age'
+	}
+
+	//now questions will be asked
+
+
+}
+
+const shoot = () => {
+
+}
+
+const train = () => {
+
+}
+
+const release = () => {
+
+}
+
+//prints corresponding interragator msg and switches to the next window
+function gameOver(status) {
+	switch (status){
+		case 'shoot':
+			shoot();
+			break;
+		case 'train':
+			train();
+			break;
+		case 'release':
+			release();
+			break;
+	}
+
+}
