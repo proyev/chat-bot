3411c26a4e41abb6ba9b4262b4a672c58003d66b
proyev <prots.yevhen@gmail.com>
2021-05-23 14:04:20 +0200

    Add jquery logic from html file

--- cipher-bot/src/logic.js
+++ cipher-bot/src/logic.js
@@ -139,3 +139,31 @@ function generateHTMLResponse(type, msg) {
 	return html;
 }
 
+//Add communication btw form and logic
+$('textarea').on('keypress', function (event){
+
+	//13 corresponds to the keypress number of 'Enter'
+	//found this line of code here: https://howtodoinjava.com/jquery/jquery-detect-if-enter-key-is-pressed/
+	if(event.which == 13){
+
+		let msg = $('textarea').val();
+
+		//input field check
+		if (msg.match(/[a-zA-Z0-9:()*&^%$#@!?]/)) {
+
+			//generates the html code for the response field
+			let userHTML = generateHTMLResponse('answer-bubble', msg);
+			$(`${userHTML}`).appendTo('main');
+
+			//to keep the viewport in place of the last messages
+			let element = document.querySelector('main');
+			element.scroll(0, element.scrollHeight);
+			$('textarea').val(null);
+
+			let botHTML = cipherThis(msg);
+			$(`${botHTML}`).appendTo('main');
+			element.scroll(0, element.scrollHeight);
+
+		}
+	}
+});
